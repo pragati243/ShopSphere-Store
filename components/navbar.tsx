c251673ce0de5b0f569aa8f683ffa7c36098f1a9
pragati243 <pragatiraja24@gmail.com>
2024-07-20 14:37:50 +0530

    changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,14 +5,12 @@ import getCategories from "@/actions/get-categories"
 import NavbarActions from "./navbar-actions"
 
 const Navbar = async () => {
-
-    const categories = await getCategories();
-
+    const categories = await getCategories()
     return (
         <div className="border-b">
             <Container>
                 <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-                    <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+                    <Link href='/' className="ml-4 flex lg:ml-0 gap-x-2">
                         <p className="font-bold text-xl">STORE</p>
                     </Link>
                     <MainNav data={categories} />
